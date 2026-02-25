56d1f9fd02f1eec64835572372213c7aa0acd307
MBANS8A1 <seanmbng@aol.com>
2026-02-25 09:41:11 +0000

    I have created a CounterContext in the Counter.js file. Next, I outlined some steps (1-4) I need to follow to complete the Compound Component Pattern (CCP).

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,8 @@
 import { createContext, useState } from "react";
-
+//1. Create the context
 const CounterContext = createContext();
+
+//2. Create the parent component
 function Counter() {
   const [count, setCount] = useState(0);
   const increase = () => setCount((c) => c + 1);
@@ -8,4 +10,7 @@ function Counter() {
 
   return <span>Counter</span>;
 }
+
+//3. Create child component(s) to help implement the common task of the overall compound component
+//4 Add the child component(s) as properties of the parent component (this step is optional)
 export default Counter;
